026a3ce56994d492190253ec9e20ee5a746faec9
Melissa249 <119982272+Melissa249@users.noreply.github.com>
2023-04-04 21:33:50 -0400

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,22 +1,16 @@
-import RPi.GPIO as GPIO
 import time
 import picamera
-
-# Set up the GPIO pins
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Create a function to take a picture
 def take_picture():
     with picamera.PiCamera() as camera:
         camera.start_preview()
         time.sleep(2)
-        camera.capture('/home/pi/image.jpg')
+        timestamp = time.strftime('%Y%m%d%H%M%S')
+        filename = '/home/pi/image_{}.jpg'.format(timestamp)
+        camera.capture(filename)
         camera.stop_preview()
-        print("Picture taken!")
+        print("Picture taken and saved as: {}".format(filename))
 
-# Wait for the button to be pressed
-while True:
-    if GPIO.input(24) == GPIO.LOW:
-        take_picture()
-        time.sleep(0.2)
+# Call the take_picture function
+take_picture()
